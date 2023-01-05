56e5b1d033877acc573ce85d0f9fe37c534f5b86
Marcelo Arias <hello@marceloarias.com>
2023-01-05 06:40:16 -0500

    Add JSDoc comments for background.js

--- background.js
+++ background.js
@@ -1,3 +1,8 @@
+/**
+ * @file Service worker script for the extension that runs when the extension is installed.
+ * {@link https://developer.chrome.com/docs/extensions/mv3/intro/mv3-overview/#service-workers}
+ */
+
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({
     shortcuts: false,
