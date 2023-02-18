28b6f9b4c895476978930f905f4b48ee776f1eb3
Marcelo Arias <hello@marceloarias.com>
2023-02-17 19:26:13 -0500

    Create utils.ts

---
+++ src/utils.ts
@@ -0,0 +1,42 @@
+/**
+ * @file Common functions used in the extension.
+ */
+
+/**
+ * @name isMac
+ * @description Checks if the user is using Mac OS
+ * @returns {boolean}
+ */
+export function isMac(): boolean {
+  return window.navigator.userAgent.indexOf("Mac") != -1;
+}
+
+/**
+ * @name getScriptOrStylesheet
+ * @description Get the URL of the script or stylesheet to be injected.
+ * @param {string} name
+ * @returns {string}
+ */
+export function getScriptOrStylesheet(name: string): string {
+  switch (name) {
+    case "greenboard.css":
+      return "https://360macky.github.io/platzikey-themes/utils/greenboard.css";
+      break;
+
+    default:
+      return "";
+      break;
+  }
+}
+
+/**
+ * Convert HTML string to HTML template.
+ * @param {String} HTML representing a single element.
+ * https://stackoverflow.com/questions/494143/creating-a-new-dom-element-from-an-html-string-using-built-in-dom-methods-or-pro/35385518#35385518
+ */
+export function htmlToElement(html: string): ChildNode | null {
+  const template = document.createElement("template");
+  html = html.trim();
+  template.innerHTML = html;
+  return template.content.firstChild;
+}
